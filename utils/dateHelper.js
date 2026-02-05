d01bd685a2a5c882e669784963eb663896eecebd
Sohail Hawaldar <sohailhawaldar24@gmail.com>
2026-02-05 10:38:52 +0530

    intial commit_05022026

--- utils/dateHelper.js
+++ utils/dateHelper.js
@@ -6,6 +6,30 @@
  * @returns {number} - Total working days (excluding weekends and holidays)
  */
 
+// exports.calculateDaysBetween = (fromDate, toDate, holidaysSet = new Set()) => {
+//   const from = new Date(fromDate);
+//   const to = new Date(toDate);
+//   let count = 0;
+
+//   const current = new Date(from);
+
+//   while (current <= to) {
+//     const dayOfWeek = current.getDay();
+//     const dateStr = current.toISOString().split("T")[0];
+
+//     const isWeekend = dayOfWeek === 0 || dayOfWeek === 6; // Sunday = 0, Saturday = 6
+//     const isHoliday = holidaysSet.has(dateStr);
+
+//     if (!isWeekend && !isHoliday) {
+//       count++;
+//     }
+
+//     current.setDate(current.getDate() + 1);
+//   }
+
+//   return count;
+// };
+
 exports.calculateDaysBetween = (fromDate, toDate, holidaysSet = new Set()) => {
   const from = new Date(fromDate);
   const to = new Date(toDate);
@@ -14,10 +38,20 @@ exports.calculateDaysBetween = (fromDate, toDate, holidaysSet = new Set()) => {
   const current = new Date(from);
 
   while (current <= to) {
-    const dayOfWeek = current.getDay();
+    const dayOfWeek = current.getDay(); 
     const dateStr = current.toISOString().split("T")[0];
 
-    const isWeekend = dayOfWeek === 0 || dayOfWeek === 6; // Sunday = 0, Saturday = 6
+    const isSunday = dayOfWeek === 0;
+
+    let isSecondOrFourthSaturday = false;
+
+    if (dayOfWeek === 6) {
+      const dayOfMonth = current.getDate();
+      const weekOfMonth = Math.ceil(dayOfMonth / 7);
+      isSecondOrFourthSaturday = weekOfMonth === 2 || weekOfMonth === 4;
+    }
+
+    const isWeekend = isSunday || isSecondOrFourthSaturday;
     const isHoliday = holidaysSet.has(dateStr);
 
     if (!isWeekend && !isHoliday) {
